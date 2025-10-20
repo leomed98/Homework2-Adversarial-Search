973191efadaf2fdb192a1eeaed352df880a03633
charles.n.phan1999@gmail.com <charles.n.phan1999@gmail.com>
2025-10-19 21:46:54 -0700

    old stuff

--- large_board_tic_tac_toe.py
+++ large_board_tic_tac_toe.py
@@ -161,7 +161,7 @@ class RandomBoardTicTacToe:
 
 
         while not done:
-            for event in pygame.event.get():  # User did something
+            for event in pygame.event.get():  
                
                if event.type == pygame.QUIT:  # If user clicked close
                    done = True  # Flag that we are done so we exit this loop
